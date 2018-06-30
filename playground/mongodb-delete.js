0ff6e648e767d359d45b98dd3dc9f8f34109ebdc
Muhammad Nadeem <muhammad.nadeem@nordicomm.co>
2018-06-30 22:12:12 +0500

    toDo delete apis

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,23 @@
+const {MongoClient, ObjectID} = require("mongodb");
+const url = "mongodb://localhost:27017";
+const dbName = "TodoApp";
+MongoClient.connect(url, (err,client) => {
+    if (err) {
+        return console.log("Error:DB Connection");
+    }
+    console.log("Success:Connected to DB");
+    const db = client.db(dbName);
+    
+    // db.collection('users').deleteMany({Name: 'Ahmad Nadeem'}).then((result) => {
+    //     console.log(result);
+    // }, (err) => {
+    //     if (err) {
+    //         console.log("Unable to delete the record", err);
+    //     }
+    // });
+
+    db.collection('users').deleteOne({Name:'Muhammad Nadeem'}).then((result)=> {
+        console.log(result);
+    });
+    client.close();
+});
